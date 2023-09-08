7b509440c453b886c537952c6a6ec6261a74cf49
Ryan Garrett <ryan@thepinkhacker.com>
2023-09-07 21:06:51 -0400

    Day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,54 @@
+// https://adventofcode.com/2022/day/1
+const FILE_PATH: &str = "day-1/resources/input";
+
+fn read_input_file() -> String {
+    std::fs::read_to_string(FILE_PATH).expect("Failed to read input file.")
+}
+
+struct Elf {
+    calories: u32,
+}
+
+fn parse_input(raw_input: &str) -> Vec<Elf> {
+    let mut elves = Vec::new();
+
+    for section in raw_input.split("\n\n") {
+        let mut calories = Vec::new();
+
+        for line in section.lines() {
+            // Trimming might not be necessary, but I'm doing it just to be safe
+            calories.push(
+                line.trim()
+                    .parse::<u32>()
+                    .expect("Failed to parse input line."),
+            );
+        }
+
+        elves.push(Elf {
+            calories: calories.iter().sum(),
+        })
+    }
+
+    elves
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw_input = read_input_file();
+    let elves = parse_input(&raw_input);
+
+    let mut max_calories = elves.get(0).expect("Expected at least one elf").calories;
+    let mut max_elf_index = 0;
+
+    for (i, elf) in elves.iter().skip(1).enumerate() {
+        if elf.calories > max_calories {
+            max_calories = elf.calories;
+            max_elf_index = i;
+        }
+    }
+
+    println!(
+        "Elf #{} has the most calories totaling at {}",
+        max_elf_index + 1,
+        max_calories,
+    );
 }
